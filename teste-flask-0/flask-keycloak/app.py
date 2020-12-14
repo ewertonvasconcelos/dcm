ff7c00fefc42b9fd97587bc34877bc5940a8d708
ewertonvasconcelos <ewerton.vasconcelos@poli.ufrj.br>
2020-12-14 02:14:44 -0300

    adding webapp commit

---
+++ teste-flask-0/flask-keycloak/app.py
@@ -0,0 +1,66 @@
+###############################################
+# Universidade Federal do Rio de Janeiro
+# Engenharia Eletrônica e da Computação
+# DCM - Data Center Manager
+# Aluno: Ewerton Vasconcelos da Silva
+# Orientador: Rodrigo de Souza Couto 
+###############################################
+#Credits to: https://github.com/verdan/flask-keycloak
+
+from flask import Flask
+from flask_oidc import OpenIDConnect
+# from flask_swagger_ui import get_swaggerui_blueprint
+
+from config import config
+from backend.utils import configure_logger
+
+app = None
+oidc = OpenIDConnect()
+_logger = None
+
+
+def create_app():
+    global app, oidc, _logger
+    app = Flask(__name__)
+
+    # Load the configurations based on the 'FLASK_ENV' environment variable
+    app.config.from_object(config)
+    
+    # Initialize logger
+    _logger = configure_logger(app)
+    # Init the OpenIDConnect application instance
+    oidc.init_app(app)
+
+    # Registers the API URLs
+    # Below import here is to prevent the circular imports
+    #from backend.apis import api as api_blueprint
+    #api_prefix = '/{}/api'.format(app.config.get('SERVICE_SLUG'))
+    #app.register_blueprint(api_blueprint, url_prefix=api_prefix)
+
+    # Registers the Views URLs
+    # This import here is to prevent the circular imports
+    from backend.views import view as view_blueprint
+    app.register_blueprint(view_blueprint)
+
+    # Registers our swagger UI blueprint
+    # swagger_docs_prefix = '{}/{}'.format(
+    #     api_prefix,
+    #     app.config.get('SWAGGER_DOCS')
+    # )
+    # swagger_spec_prefix = '{}/{}'.format(
+    #     api_prefix,
+    #     app.config.get('SWAGGER_SPEC')
+    # )
+    # swaggerui_blueprint = get_swaggerui_blueprint(
+    #     swagger_docs_prefix,
+    #     swagger_spec_prefix,
+    #     config={
+    #         'app_name': app.config.get('SWAGGER_NAME')
+    #     },
+    # )
+    # app.register_blueprint(
+    #     swaggerui_blueprint,
+    #     url_prefix=swagger_docs_prefix
+    # )
+
+    return app
---
+++ teste-flask-0/flask-keycloak/backend/views.py
@@ -0,0 +1,84 @@
+from flask import Blueprint, request, redirect, url_for, render_template, flash
+from werkzeug.utils import secure_filename
+from app import oidc, _logger, app
+import logging
+import os, time
+
+view = Blueprint('view', __name__)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+#{'exp': 1607915952, 'iat': 1607915652, 'auth_time': 1607914183, 'jti': 'f34dd21a-b5b4-4593-94ea-c73813805618', 'iss': 'http://192.168.1.2:8080/auth/realms/flask-demo', 'aud': 'flask-client', 'sub': '3aace208-029b-4e41-a499-265a15a35b19', 'typ': 'ID', 'azp': 'flask-client', 'session_state': '6d6b4df0-a558-442b-8176-89a19aa1d329', 'at_hash': 'bDfiUj2l9qVaesCNN97-sw', 'acr': '0', 'email_verified': False, 'name': 'Ewerton Silva', 'preferred_username': 'ewerton', 'given_name': 'Ewerton', 'locale': 'pt-BR', 'family_name': 'Silva', 'email': 'ewerton.vasconcelos@poli.ufrj.br'}
+
+
+@view.route('/')
+def index():
+    if oidc.user_loggedin:
+        #return 'Logged In Home Page - Hi {}. <a href="/logout">Logout</a>'.format(oidc.user_getfield('name'))
+        #(['preferred_username', 'email', 'sub'])
+        #print(oidc.user_getfield('preferred_username'))
+        return render_template('index.html', name=oidc.user_getfield('name'),sub=oidc.user_getfield('sub')+'?dummy=' + str(time.time()))
+
+    else:
+        return redirect(url_for('view.login'))
+
+
+@view.route('/login')
+@oidc.require_login
+def login():
+    _logger.info(
+        '{} logged in successfully'.format(oidc.user_getfield('email')))
+    return redirect(url_for('view.index'))
+
+
+@view.route('/logout')
+@oidc.require_login
+def logout():
+    email = oidc.user_getfield('email')
+    oidc.logout()
+    redirect_url = request.url_root.strip('/')
+    keycloak_issuer = oidc.client_secrets.get('issuer')
+    keycloak_logout_url = '{}/protocol/openid-connect/logout'.format(
+        keycloak_issuer
+    )
+    _logger.info('{} logged out'.format(email))
+
+    return redirect('{}?redirect_uri={}'.format(
+        keycloak_logout_url,
+        redirect_url)
+    )
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@view.route('/profile', methods=['GET', 'POST'])
+def upload_file():
+    if oidc.user_loggedin:
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                #filename = secure_filename(file.filename)
+                filePath = os.path.abspath(os.getcwd()) + app.config['UPLOAD_FOLDER']
+                filename = oidc.user_getfield('sub')
+                print(filePath + filename)
+
+                if os.path.exists(filePath + filename):
+                    os.remove(filePath+filename)
+
+                file.save(os.path.join(filePath,oidc.user_getfield('sub')))
+                return render_template('profile.html',sub=oidc.user_getfield('sub'))
+
+        return render_template('profile.html', sub=oidc.user_getfield('sub')+'?dummy=' + str(time.time()), name=oidc.user_getfield('name'))
+    else:
+        return redirect(url_for('view.login'))
